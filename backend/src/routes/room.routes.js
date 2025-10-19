59c35f21fe680739cfcf7e3fe9da71a31e1d1aa7
Bemnet kassa Bekele <bemnetkassa79@gmail.com>
2025-10-19 22:58:33 +0300

    the room routes file

--- backend/src/routes/room.routes.js
+++ backend/src/routes/room.routes.js
@@ -1,8 +1,19 @@
 import express from "express";
-import { register, login } from "../controllers/auth.Controller.js";
+import {
+  getRooms,
+  getRoomById,
+  createRoom,
+  updateRoom,
+  deleteRoom,
+} from "../controllers/room.controller.js";
+import { protect } from "../middlewares/auth.middleware.js";
+
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.get("/", getRooms);
+router.get("/:id", getRoomById);
+router.post("/", protect, createRoom);
+router.put("/:id", protect, updateRoom);
+router.delete("/:id", protect, deleteRoom);
 
 export default router;
