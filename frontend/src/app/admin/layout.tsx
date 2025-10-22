bb2f09a146a4576df35d7504c78f980a60ad1b90
Bemnet kassa Bekele <bemnetkassa79@gmail.com>
2025-10-22 22:41:18 +0300

    layout update for the admin page login

--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -1,14 +1,12 @@
-import AdminSidebar from "@/components/AdminSideBar";
-import AdminHeader from "@/components/AdminHeader";
+"use client";
 
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex min-h-screen bg-gray-100">
-      <AdminSidebar />
-      <div className="flex-1 flex flex-col">
-        <AdminHeader />
-        <main className="p-6">{children}</main>
-      </div>
-    </div>
-  );
+import React from "react";
+import { AdminAuthProvider } from "@/context/adminAuthContext"; // adjust if needed
+
+export default function AdminRootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <AdminAuthProvider>{children}</AdminAuthProvider>;
 }
