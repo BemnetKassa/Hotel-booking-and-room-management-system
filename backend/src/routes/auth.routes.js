35b5c6e8ab7473e88819858a518cb0fe4863f544
Bemnet kassa Bekele <bemnetkassa79@gmail.com>
2025-11-04 15:30:43 +0300

    based on the updated auth controller we need  modify the routes  too

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { register, login } from "../controllers/auth.Controller.js";
+import { register, login, adminLogin } from "../controllers/auth.Controller.js";
+
 const router = express.Router();
 
+// User
 router.post("/register", register);
 router.post("/login", login);
+
+// Admin
+router.post("/admin/login", adminLogin);
 
 export default router;
